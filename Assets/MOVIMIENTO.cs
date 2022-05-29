3189af0ec597b073b000d0d06daa6b97d8b916ea
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-05-29 20:13:16 -0300

    Initial commit

---
+++ Assets/MOVIMIENTO.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVIMIENTO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speedZ;
+    public float speedgiro;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, speedZ);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -speedZ);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, speedgiro, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0,-speedgiro, 0);
+        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    if(transform.position.y < 2.6)
+        //    {
+        //        transform.Translate(0, 1f, 0);
+                
+        //    }
+            
+
+        //}
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+            
+        //    if (transform.position.y > 0.54)
+        //    {
+        //        transform.Translate(0, -1f, 0);
+        //    }
+
+        //}
+    }
+}
---
+++ Assets/popiss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popiss : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+    public GameObject caquita;
+    public GameObject coneji;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(caquita);
+            caquita.transform.position = coneji.transform.position;
+        } 
+       
+    }
+    
+}
